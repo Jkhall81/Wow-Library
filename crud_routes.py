efddb0679c22ea1768c0e54ed010ad0416410dff
Jkhall81 <jason.kei.hall@gmail.com>
2023-08-15 14:23:40 -0700

    fixed issue where comment posted by was showing current user and not the user who actually made the post.  did a little back end spell casting and fixed that business.  Also now different users can comment on the same book in a users collection.

--- crud_routes.py
+++ crud_routes.py
@@ -2,7 +2,7 @@ from datamanager.sqlalchemy_data_manager import SQAlchemyDataManager
 from flask import Blueprint, flash, redirect, render_template, request, send_from_directory, url_for
 from flask_login import current_user
 from forms.forms import AddBookForm, RatingForm, BookCommentForm, EditCommentForm
-from models import Book, Comment, db
+from models import Book, Comment, User, db
 
 crud_bp = Blueprint('crud', __name__)
 data_manager = SQAlchemyDataManager('data/library.sqlite')
@@ -24,9 +24,9 @@ def add_book(user_id):
         new_book = data_manager.add_book(user_id, api_params)
         if new_book:
             flash('New book successfully added!')
-            return redirect(url_for('crud.my_books', user_id=current_user.id))
+            return redirect(url_for('crud.my_books', user_id=current_user.id, user=current_user))
 
-    return render_template('add_book.html', form=form)
+    return render_template('add_book.html', form=form, user=current_user)
 
 
 @crud_bp.route('/delete/<int:book_id>', methods=['POST'])
@@ -51,7 +51,7 @@ def my_books(user_id):
     for book in books:
         average_ratings[book.id] = data_manager.average_rating(book.id)
 
-    return render_template('my_books.html', books=books, form=form, average_ratings=average_ratings)
+    return render_template('my_books.html', books=books, form=form, average_ratings=average_ratings, user=current_user)
 
 
 @crud_bp.route('/rate_book/<int:book_id>', methods=['POST', 'GET'])
@@ -72,7 +72,6 @@ def book_details(book_id):
     book = Book.query.filter_by(id=book_id).first()
     form = BookCommentForm(request.form)
     comments = Comment.query.filter_by(book_id=book_id).all()
-    first_name, last_name = data_manager.get_name(current_user.id)
     if request.method == 'POST' and form.validate():
         subject = form.subject.data
         comment_text = form.comment_text.data
@@ -80,9 +79,18 @@ def book_details(book_id):
         new_comment = data_manager.add_comment(subject, comment_text, book_id, current_user.id)
         if new_comment:
             flash('Comment successfully submitted!')
-            return redirect(url_for('crud.book_details', book_id=book.id, book=book, form=form, comments=comments))
+            return redirect(url_for('crud.book_details', book_id=book.id, book=book, form=form, comments=comments, user=current_user))
 
-    return render_template('book_details.html', book=book, form=form, comments=comments, first_name=first_name, last_name=last_name)
+    comments_with_user_info = []
+    for comment in comments:
+        # watch me work
+        user = User.query.get(comment.user_id)
+        comments_with_user_info.append({
+            'comment': comment,
+            'user': user,
+        })
+
+    return render_template('book_details.html', book=book, form=form, comments=comments_with_user_info, user=current_user)
 
 
 @crud_bp.route('/edit_comment/<int:comment_id>', methods=['POST', 'GET'])
@@ -98,9 +106,9 @@ def edit_comment(comment_id):
 
         db.session.commit()
         flash('Comment successfully updated!')
-        return redirect(url_for('crud.book_details', book_id=comment.book_id))
+        return redirect(url_for('crud.book_details', book_id=comment.book_id, user=current_user))
 
-    return render_template('edit_comment.html', comment_id=comment_id, form=form, comment=comment)
+    return render_template('edit_comment.html', comment_id=comment_id, form=form, comment=comment, user=current_user)
 
 
 @crud_bp.route('/all_users')
